43df85b13010d08b5560bf088668d8a735c28ff4
Alejandro González Damin <44491214+agd@users.noreply.github.com>
2019-11-02 12:52:42 +0100

    ARRAY TABLA

--- TEMA2/Arrays/Ej4.php
+++ TEMA2/Arrays/Ej4.php
@@ -17,37 +17,39 @@ and open the template in the editor.
             array("Deporte"=> "FIFA", "PES", "MOTO G")
             );
         ?>
-        <table border="1">
-            <thead>
-                <tr>
-                    <th><?php echo $array["Accion"][0]?></th>
-                    <th><?php echo $array[4]?></th>
-                    <th><?php echo $array[8]?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td><?php echo $array[1]?></td>
-                    <td><?php echo $array[5]?></td>
-                    <td><?php echo $array[9]?></td>
-                </tr>
-                <tr>
-                    <td><?php echo $array[2]?></td>
-                    <td><?php echo $array[6]?></td>
-                    <td><?php echo $array[10]?></td>
-                </tr>
-                <tr>
-                    <td><?php echo $array[3]?></td>
-                    <td><?php echo $array[7]?></td>
-                    <td><?php echo $array[11]?></td>
-                </tr>
-                <tr>
-                    <td><?php echo $array[0]?></td>
-                    <td><?php echo $array[0]?></td>
-                    <td><?php echo $array[0]?></td>
-                </tr>
-            </tbody>
-        </table>
+        <table width="100%" border="1">
 
+
+
+         <?php
+
+           foreach($tipo as $genero => $value)
+              {
+
+
+         ?>
+            <tr> 
+                   <th>
+                       <?php echo key($value); ?>
+                 </th>
+                         <?php foreach($value as $genero=>$value)
+                           {
+                         ?>
+                  <td>
+                        <?php echo $value;?>
+                 </td>
+                        <?php
+                         }
+                        ?>
+            </tr>
+
+
+
+
+        <?php
+            }
+           ?>
+
+        </table>
     </body>
 </html>
