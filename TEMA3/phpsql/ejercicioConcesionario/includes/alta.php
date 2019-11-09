d39fc6de669516004f7768d738462f2491e7fc70
Alejandro González Damin <44491214+agd@users.noreply.github.com>
2019-11-09 11:10:58 +0100

    ejercicio concesionario

--- TEMA3/phpsql/ejercicioConcesionario/includes/alta.php
+++ TEMA3/phpsql/ejercicioConcesionario/includes/alta.php
@@ -10,46 +10,48 @@
         <?php include 'navegacion.php';?>
         <main>
             <h2>Este es el alta</h2>
-            <?php if (!isset($_POST[''])){?>
+
              <form action="alta.php" method="POST">
-        <div>
-        <label>Marca: </label>
-        <input type="text" name="marca" maxlength="50">
-        </div>
-                 
-        <div>
-        <label>Modelo: </label>
-        <input type="text" name="modelo" maxlength="100">
-        </div>
+                <div>
+                    <label>Marca: </label>
+                    <input type="text" name="marca" maxlength="50">
+                </div>
 
-        <div>
-        <label>Precio: </label>
-        <input type="number" name="precio" maxlength="10">
-        </div>
+                <div>
+                    <label>Modelo: </label>
+                    <input type="text" name="modelo" maxlength="100">
+                </div>
 
-        <div>
-        <label>Stock: </label>
-        <input type="number" name="stock" maxlength="20">
-        </div>
+                <div>
+                    <label>Precio: </label>
+                    <input type="number" name="precio" maxlength="10">
+                </div>
 
-        <div>
-        <input type="submit">
-        </div>
+                <div>
+                    <label>Stock: </label>
+                    <input type="number" name="stock" maxlength="20">
+                </div>
 
-    </form>
+                <div>
+                    <input type="submit" name="submit">
+                </div>
+
+            </form>
         <?php
-            }else{
+            if(isset($_POST['submit'])){
             include 'Conexion.php';
-            $id="NULL";
             $modelo=$_POST['modelo'];
             $marca=$_POST['marca'];
-            $precio=int($_POST['precio']);
-            $stock=int($_POST['stock']);
-            $sql="insert into coches values('$id','$modelo','$marca',$precio,$stock)";
-            $conn->exec($sql);
-            
+            $precio=(int)$_POST['precio'];
+            $stock=(int)$_POST['stock'];
+            $sql="insert into coches values(null,'$modelo','$marca','$precio','$stock')";
+            $insert= mysqli_query($conexion, $sql);
+            mysqli_close($conexion);
+            header("Locatio: index.php");
         }
-        mysqli_close($conexion);
+        
+     
+                
         ?>
         </main>
         
